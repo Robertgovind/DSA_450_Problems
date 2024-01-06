a4c9d407823f26a577f0d6afc9a6ff5aea151a6f
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-01-06 08:58:34 +0545

    problem 1

--- 01_reverse_array.cpp
+++ 01_reverse_array.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
+void reverseArray(int arr[],int n){
+    int s=0,e=n-1;
+    while(s<e){
+        swap(arr[s++],arr[e--]);
+    }
+}
+
 int main(){
-    cout<<"Hello World";
+    int arr[]={1,2,3,4,5};
+    reverseArray(arr,5);
+    for(int i:arr)
+    cout<<i<<" ";
     return 0;
 }
