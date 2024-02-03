0aaa1efa90dff5c800d36a24cb22daaad2daf701
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-02-03 23:31:46 +0545

    dsa sheet

--- 03_Kth_max_min.cpp
+++ 03_Kth_max_min.cpp
@@ -2,14 +2,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//Give Time Limit Error(TLE)
-int kthSmallest(int arr[], int l, int r, int k) {
-        //code here
-       
-    }
+int kthSmallest(int *arr, int l, int r, int k) {
+       sort(arr,arr+(r+1));
+       return arr[k-1];
+}
+int kthLargest(int *arr, int l, int r, int k) {
+       sort(arr,arr+(r+1));
+       return arr[r-k];
+}
 
 int main(){
     int arr[]={7, 10, 4, 3, 20, 15};
-    cout<<kthSmallest(arr,0,5,3);
+    cout<<kthSmallest(arr,0,5,3)<<endl;
+    for(int i=0;i<6;i++)
+    cout<<arr[i]<<" ";
     return 0;
 }
