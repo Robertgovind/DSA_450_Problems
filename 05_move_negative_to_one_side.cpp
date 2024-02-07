a00294ad07839e9c8fa30c177911b768c1887863
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-02-07 23:10:07 +0545

    dsa sheet 05

--- 05_move_negative_to_one_side.cpp
+++ 05_move_negative_to_one_side.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 void segregateElements(int arr[],int n)
     {
@@ -16,4 +17,28 @@ void segregateElements(int arr[],int n)
         for(int i=0;i<n;i++){
             arr[i]=temp[i];
         }
+}
+
+void moveNegatives(vector<int>& nums){
+    int n = nums.size();
+    int s = 0;
+    int e = n-1;
+    while(s<=e){
+        if(nums[e] < 0){
+            e--;
+        }
+        if(nums[s] < 0){
+            swap(nums[s++],nums[e--]);
+        }
+        else{
+            s++;
+        }
     }
+}
+int main(){
+    vector<int> ans = {-2, 9,3,-1,5,-3,9,-6};
+    moveNegatives(ans);
+    for(int i: ans)
+    cout<<i<<" ";
+    return 0;
+}
