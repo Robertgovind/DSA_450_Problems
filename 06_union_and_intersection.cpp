55e4705e4ab768246e2fd47263fd4924f07eadb2
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-02-12 21:43:14 +0545

    dsa sheet

--- 06_union_and_intersection.cpp
+++ 06_union_and_intersection.cpp
@@ -1,7 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int doUnion(int a[], int n, int b[], int m)  {
+        //code here
+        sort(a,a+n);
+        sort(b,b+m);
+        int firstIndex = 0;
+        int secondIndex = 0;
+        int count = 0;
+        while(firstIndex<n && secondIndex<m){
+            if(a[firstIndex] == b[secondIndex]){
+                firstIndex++;
+                secondIndex++;
+            }
+            else if(a[firstIndex]<b[secondIndex]){
+                firstIndex++;
+                count++;
+            }
+            else if(a[firstIndex]>b[secondIndex]){
+                secondIndex++;
+                count++;
+            }
+        }
+        while(firstIndex<n){
+            count++;
+            firstIndex++;
+        }
+        while(secondIndex<m){
+            count++;
+            secondIndex++;
+        }
+    return count;
+}
 
+int main(){
+    int arr1[]={85, 25, 1, 32, 54, 6};
+    int arr2[]={85, 2 };
+    cout<<doUnion(arr1,6,arr2,4);
     return 0;
 }
