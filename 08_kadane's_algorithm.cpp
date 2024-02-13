778eda6d5271d50a2e0ba388c8c1d8e74d9f540a
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-02-14 00:33:44 +0545

    dsa sheet

---
+++ 08_kadane's_algorithm.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+long long maxSubarraySum(int arr[], int n){
+        long long int currSum = 0;
+        long long int maxSum = INT_MIN;
+        for(int i= 0;i<n;i++){
+            currSum += arr[i];
+            if(currSum>maxSum){
+                maxSum = currSum;
+            }
+            
+            if(currSum<0){
+                currSum = 0;
+            }
+        }
+        return maxSum;
+}
