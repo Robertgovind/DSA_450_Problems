1395dffdb485c3dff8de3d68f380d1e5c2a82738
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-02-13 23:42:29 +0545

    dsa sheet

---
+++ 07_rotate_by_one.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+void rotate(int arr[], int n)
+{
+    int a[n];
+    for(int i=0;i<n;i++){
+        a[(i+1)%n] = arr[i];
+    }
+    for(int i=0;i<n;i++){
+        arr[i]=a[i];
+    }
+}
