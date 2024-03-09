c6e321aa31a04e5a7c09d7645122307923c5cc81
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-03-10 03:18:50 +0545

    reverse a string using stack

---
+++ 280_reverse_a_string.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stack>
+//return the address of the string
+char* reverse(char *S, int len)
+{
+    //code here
+    stack<int> s;
+    char *str;
+    str = new char[len];
+    for(int i=0;i<len;i++){
+        s.push(S[i]);
+    }
+    for(int i=0;i<len;i++){
+        str[i]=s.top();
+        s.pop();
+    }
+    return str;
+}
