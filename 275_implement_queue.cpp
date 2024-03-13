49ad7fb824387fc45aafd654ad6fe50479d2a4c5
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-03-14 01:47:23 +0545

    implement stack using array

---
+++ 275_implement_queue.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+//The structure of the class is
+class MyQueue {
+private:
+    int arr[100005];
+    int front;
+    int rear;
+
+public :
+    MyQueue(){front=0;rear=0;}
+    void push(int);
+    int pop();
+};
+ 
+
+//Function to push an element x in a queue.
+void MyQueue :: push(int x)
+{
+        // Your Code
+        
+            arr[rear]=x;
+            rear++;
+        
+        
+}
+
+
+//Function to pop an element from queue and return that element.
+int MyQueue :: pop()
+{
+        // Your Code
+        int d;
+        if(front == rear){
+            return -1;
+        }
+        else{
+            d= arr[front];
+            front++;
+            if(front == rear){
+                front =0;
+                rear=0;
+            }
+            return d;
+        }
+}
